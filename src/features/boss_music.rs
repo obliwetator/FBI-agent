d9613e6ba32f30614452db637a279075fb26ba50
patryk styla <43710236+obliwetator@users.noreply.github.com>
2022-12-09 02:49:29 +0000

    uneeded

--- src/features/boss_music.rs
+++
@@ -1,5 +0,0 @@
-pub async fn get_user_boss_music() {}
-
-pub async fn add_user_boss_music() {}
-
-pub async fn update_user_boss_music() {}
--- src/features/mod.rs
+++
@@ -1,3 +0,0 @@
-// #![allow(dead_code, unused_variables)]
-
-pub mod boss_music;
