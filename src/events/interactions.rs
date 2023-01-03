a2ea844de213af62cda4486e0fcccb1d419531d3
patryk styla <43710236+obliwetator@users.noreply.github.com>
2023-01-03 20:29:52 +0000

    basic interaction logic(hardcoded)

--- src/events/interactions.rs
+++ src/events/interactions.rs
@@ -1,7 +1,12 @@
 use serenity::{
-    model::prelude::interaction::{Interaction, InteractionResponseType},
+    model::prelude::interaction::{
+        application_command::ApplicationCommandInteraction, Interaction, InteractionResponseType,
+    },
     prelude::*,
 };
+use songbird::input::Restartable;
+
+use super::voice::RECORDING_FILE_PATH;
 
 pub async fn interaction_create(ctx: Context, interaction: Interaction) {
     match interaction {
@@ -9,6 +14,7 @@ pub async fn interaction_create(ctx: Context, interaction: Interaction) {
         Interaction::ApplicationCommand(application_command) => {
             let content = match application_command.data.name.as_str() {
                 "help" => ":(".to_string(),
+                "play" => handle_play_audio(&application_command, &ctx, "").await,
                 _ => format!(
                     "Unkown application_command with the name {}",
                     application_command.data.name
@@ -30,6 +36,34 @@ pub async fn interaction_create(ctx: Context, interaction: Interaction) {
         Interaction::Autocomplete(_) => todo!(),
         Interaction::ModalSubmit(_) => todo!(),
     }
+}
+
+pub async fn handle_play_audio(
+    application_command: &ApplicationCommandInteraction,
+    ctx: &Context,
+    file_name: &str,
+) -> String {
+    let manager = songbird::get(ctx).await.unwrap();
+    // let result =songbird::ffmpeg(format!("{}{}", RECORDING_FILE_PATH, "/projects/FBI-agent/voice_recordings/362257054829641758/2022/December/1670620473631-161172393719496704-QazZ.ogg")).await.unwrap();
+
+    // TODO: This does not return and error if the wrong file path is given?
+    let result = Restartable::ffmpeg(
+        format!(
+            "{}{}",
+            RECORDING_FILE_PATH,
+            "/362257054829641758/2022/December/1670620473631-161172393719496704-QazZ.ogg"
+        ),
+        false,
+    )
+    .await
+    .unwrap();
+
+    let input = songbird::input::Input::from(result);
+    let handler = manager.get(application_command.guild_id.unwrap()).unwrap();
+    let handler_lock = handler.lock().await.enqueue_source(input);
+    let _ = handler_lock.set_volume(0.5);
+
+    "jamming".to_string()
 }
 
 // pub async fn application_command_create(_ctx: Context, _application_command: ApplicationCommand) {
